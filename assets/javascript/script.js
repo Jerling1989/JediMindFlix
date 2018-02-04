20c7c267e8627b04b7f33ca2b54d1e1f09871828
Jacob <jerling1989@gmail.com>
2018-02-04 15:53:15 -0500

    JS comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,24 @@
-// Variables
+// VARIABLES
 var yodaSummary = "";
 var summary = "";
 var title ="";
 
-// Whent title is submitted
+// CLICK EVENT FOR WHEN TITLE IS SUBMITTED
 $("#check-movie").on("click", function(event) {
   event.preventDefault();
 
   $('#default_screen').remove();
 
-  // Clear last message
+  // CLEAR LAST MESSAGE
   $("#netflix").empty();
   $("#yodaSpeak").empty();
   $("#ratingDisplay").empty();
 
 
-  // This line grabs the input from the textbox
+  // THIS LINE GRABS THE INPUT FROM THE TEXTBOX
   title = $("#title-input").val().trim();
 
-  // Run netflix API
+  // RUN NETFLIX API
   var queryURLtitle = "https://netflixroulette.net/api/api.php?title=" + title;
 
   var key = "bh5PH4M5r6mshqeGupreXyWUwOCzp1gPcqajsnF7sxA3FjgZkO";
@@ -33,23 +33,23 @@ $("#check-movie").on("click", function(event) {
   }).done(function(response) {
     console.log(response);
 
-  // Storing the title, ratings, poster and summary
+  // STORING THE TITLE, RATINGS, POSTER AND SUMMARY
   var showTitle = response.show_title;
   var netflixRating = response.rating;
   var posterURL = response.poster;
   summary = response.summary;
 
-  // Creating an element to hold the title and ratings
+  // CREATING AN ELEMENT TO HOLD THE TITLE AND RATINGS
   var printTitle = $("<h3>").text("Watch " + showTitle + " on Netflix, you can. Hmmm.");
   var printSummary = "<div class=\"row\"><div class=\"col m3\"><img height=\"200px\" src=\"" + posterURL + "\" alt=\"" + title + "\" /></div><div id='plot' class=\"col m9\"><br><br><br><span id=\"printSummary\"></span></div></div>";
   var printRating = "<table id=\"ratings-table\"><thead><tr><th><div id='table_title'>Judge me by my ratings, do you?</div></th></tr></thead><tbody><tr><td><img id='saber' src='assets/images/lightSaberGreen.png'>&nbsp;&nbsp;&nbsp; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - &nbsp;&nbsp;&nbsp;Netflix rating: " + netflixRating + "</td></tr></tbody></table>";
 
-  // Appending the title and rating. 
+  // APPENDING THE TITLE AND RATING
   $("#netflix").append(printTitle);
   $("#yodaSpeak").append(printSummary);
   $("#ratingDisplay").append(printRating);
 
-  // Run Yoda Speak API >> Send resulting summary to yoda.
+  // RUN YODA SPEAK API >> SEND RESULTING SUMMARY TO YODA
   var queryURLyoda = "https://yoda.p.mashape.com/yoda?sentence=" + summary;
 
     $.ajax({
@@ -65,19 +65,19 @@ $("#check-movie").on("click", function(event) {
 
       var yodaSummary = response;
 
-      // Appending the summary
+      // APPEDNING THE SUMMARY
       $("#printSummary").append(yodaSummary);
       
       }).fail(function(){
       yodaSummary = summary;
 
-      // Appending the summary as is
+      // APPENDING THE SUMMARY AS IS
       $("#printSummary").append(yodaSummary);
 
       });
-      // End of yoda ajax call
+      // END OF YODA AJAX CALL
 
-    // Run www.omdbapi.com API to pull in ratings
+    // RUN WWW.OMDBAPI.COM API TO PULL IN RATINGS
     var ratingQueryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece";
     console.log(ratingQueryURL);
     $.ajax({
@@ -86,43 +86,43 @@ $("#check-movie").on("click", function(event) {
     }).done(function(response) {
       console.log(response.Ratings);
 
-      // Check length of rating array
+      // CHECK LENGTH OF RATING ARRAY
       for (i=0;i<response.Ratings.length;i++) {
 
-        // Check to see if one of them is a Netflix rating... Do not use Netflix
+        // CHECK TO SEE IF ONE OF THEM IS A NETFLIX RATING... DO NOT USE NETFLIX
         if (response.Ratings[i].Source == "Netflix") {
 
         console.log("There is already a Netflix rating.");
 
       } else {
 
-        // Add ratings data into the table
+        // ADD RATINGS DATA INTO THE TABLE
         $("#ratings-table > tbody").append("<tr><td><img id='saber' src='assets/images/lightSaberGreen.png'>&nbsp;&nbsp;&nbsp; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - &nbsp;&nbsp;&nbsp;" + response.Ratings[i].Source + ": " + response.Ratings[i].Value + "</td><td>");
 
           }
         }
       });
-    // End movie rating loop
+    // END MOVIE RATING LOOP
 
-    // Clear input field
+    // CLEAR INPUT FIELD
     title = $("#title-input").val("");
         
     }).fail(function(){
 
-      // Creating an error message
+      // CREATING AN ERROR MESSAGE
       var printError = $("<h3>").text("Watch this on Netflix, you cannot.");
       var errorImage = "<div id=\"errorImage\"><img src=\"assets/images/fail.gif\" alt=\"Luke Skywalker screams, No!\"/></div><br>";
       printError.append(errorImage);
 
-      // Appending the title
+      // APPENDING THE TITLE
       $("#netflix").append(printError);
 
-      // Clear input field
+      // CLEAR INPUT FIELD
       title = $("#title-input").val("");
 
       });
-      // End of Netfix Roulette ajax call
+      // END OF NETFLIX ROULETTE AJAX CALL
   });
-  // End of button on function
+  // END OF BUTTON ON FUNCTION
 
      
